e70ae3931164673f88770d5603df8646c03ebdf6
Reid Hewitt <sam.hew.r@gmail.com>
2023-05-23 16:43:48 -0600

    saving progress.

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -18,14 +18,20 @@ def test_create_table(create_test_table_sql, get_test_table_sql):
 
     success = False
 
+    # config_file = {
+    #             "host": os.getenv("POSTGRES_HOST"),
+    #             "user": os.getenv("POSTGRES_USER"),
+    #             "password": os.getenv("POSTGRES_PASSWORD"),
+    #         }
+
+    config_file = { 
+        "host": "db", 
+        "user": "postgres_user",
+        "password": "postgres_password"
+    }
+
     try:
-        conn = psycopg2.connect(
-            **{
-                "host": os.getenv("TEST_POSTGRES_HOST"),
-                "user": os.getenv("TEST_POSTGRES_USER"),
-                "password": os.getenv("TEST_POSTGRES_PASSWORD"),
-            }
-        )
+        conn = psycopg2.connect(**config_file)
         curs = conn.cursor()
         curs.execute(create_test_table_sql)  # create the table
         curs.execute(get_test_table_sql)
